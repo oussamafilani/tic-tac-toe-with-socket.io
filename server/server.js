017be23c1c7ff778dce35708689c6df62b17e4a8
Oussama Filani <42185573+filani07@users.noreply.github.com>
2022-03-28 01:06:25 +0000

    Create the server with socket.io

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const io = require("socket.io")(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("User Connected");
+
+  socket.on("joinRoom", (roomCode) => {
+    console.log(`A user joined the room ${roomCode}`);
+    socket.join(roomCode);
+  });
+
+  socket.on("play", ({ id, roomCode }) => {
+    console.log(`play at ${id} to ${roomCode}`);
+    socket.broadcast.to(roomCode).emit("updateGame", id);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected");
+  });
+});
+
+server.listen(5000, () =>
+  console.log("server running on http://localhost:5000")
+);
